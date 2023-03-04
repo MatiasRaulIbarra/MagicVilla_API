ef030dd4ead0a052ef044e724e0f6e8cee5192cf
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2023-03-04 20:48:42 -0300

    validación más codigo de estado

---
+++ MagicVilla/MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,38 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("id:int")]
+        public  ActionResult< VillaDto> GetVilla(int id)
+        {
+
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa= VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+    }
+}
